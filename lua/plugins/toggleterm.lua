5f83d0916feac7cef47cb440550444e1113d897a
EricYang123 <eyang420@gmail.com>
2026-01-21 18:33:28 -0700

    made the default terminal vertical

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,6 +3,7 @@ return {
 	'akinsho/toggleterm.nvim', 
 	config = function()
 	    require("toggleterm").setup{
+		direction = "vertical",
 		open_mapping = [[<c-\>]],
 		size = function(term)
 		    if term.direction == "horizontal" then
