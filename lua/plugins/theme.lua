c7b15c73dbe123bf82cc77e4c74262f534d0a279
Eric Yang <eyang@DESKTOP-N5RI92O.ht.home>
2026-03-23 20:26:47 -0600

    Changed Theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -35,4 +35,16 @@ return {
 		vim.cmd("colorscheme onedark_dark")
 	    end,
 	},
+
+	{
+	    "scottmckendry/cyberdream.nvim",
+	    lazy = false,
+	    priority = 1000,
+	    config = function()
+		require("cyberdream").setup({
+		    transparent = true,
+		})
+		vim.cmd("colorscheme cyberdream")
+	    end,
+    },
 }
